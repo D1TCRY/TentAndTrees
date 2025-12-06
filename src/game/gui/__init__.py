603b1c8441ac29c06df932f1b729d384aaec1b34
Diego Cecchelani <134800819+D1TCRY@users.noreply.github.com>
2025-12-06 11:09:27 +0100

    Delete src directory

--- src/game/gui/__init__.py
+++
@@ -1,6 +0,0 @@
-from .gui_component import GUIComponent
-from .bar import Bar
-from .button import Button
-from .text import Text
-from .color import Color
-from .board import Board
--- src/game/state/__init__.py
+++
@@ -1,4 +0,0 @@
-from .action import Action
-from .app_phase import AppPhase
-from .cell_state import CellState
-from .menu_phase import MenuPhase
