97e3a428d61af5e8d7d747eb92f428f736353540
Diego Cecchelani <134800819+D1TCRY@users.noreply.github.com>
2025-12-08 11:11:51 +0100

    Delete src directory

--- src/game/__init__.py
+++
@@ -1,5 +0,0 @@
-from .core import *
-from .state import *
-from .board_game import *
-from .board_game_gui import *
-from .gui import *
--- src/game/core/__init__.py
+++
@@ -1,4 +0,0 @@
-from .game import *
-from .file_management import *
-from .app import *
-from .menu_manager import *
